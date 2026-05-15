39b0da4347ace51a8f6b6656f15dccc6ac03f284
Minjae Kim <minjae010901@gmail.com>
2026-05-15 14:28:51 +0900

    feat: add alarm modal UI component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,11 @@
+// AlarmModal.vue 임시 확인용으로 추가해둔 코드 (실제로는 App.vue에서 AlarmModal 사용X)
+<script setup>
+import AlarmModal from './components/AlarmModal.vue'
+</script>
+
 <template>
+  <div style="padding: 40px; background: #f5f7fb; min-height: 100vh;">
+    <AlarmModal />
+  </div>
   <RouterView />
 </template>
